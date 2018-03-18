4e0e634c2499a4e0c611c1c14ba68c5f7a52d3f9
megane42 <h.kzm.gm@gmail.com>
2018-03-19 00:12:41 +0900

    add textbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import QRCode               from 'qrcode.react';
 import './App.css';
 
+class QRCodeBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { text: props.value };
+    }
+    
+    changeText = (e) => {
+        this.setState({
+            text: e.target.value
+        });
+    }
+    
+    render() {
+        let text = this.state.text;
+        return (
+                <div className="qrcode_box">
+                    <QRCode value={text} />
+                    <input type="text" value={text} onChange={this.changeText} />
+                </div>
+        )
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <QRCode value="http://facebook.github.io/react/" />
-    );
-  }
+    render() {
+        return (
+                <QRCodeBox value="http://facebook.github.io/" />
+        );
+    }
 }
 
 export default App;
